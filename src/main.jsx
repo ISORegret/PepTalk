d0ec5855a211ff742c989086c2446a91cb7fecd4
ISORegret <rtaylorbrick@gmail.com>
2026-08-28 22:14:27 -0400

    Hotfix unstable structural redesign layer

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx'
 import { SupabaseAuthProvider } from './context/SupabaseAuthContext.jsx'
 import './index.css'
 import './readability-overrides.css'
-import './structural-redesign.css'
 import './qol-enhancements.js'
-import './structural-redesign.js'
 
 /**
  * GitHub Pages and other static hosts often build without VITE_* secrets.
