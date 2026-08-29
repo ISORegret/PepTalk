d2fc45f8644da3011616fa6029ae93da80e64f94
ISORegret <rtaylorbrick@gmail.com>
2026-08-28 23:55:20 -0400

    Enable PepTalk 3.0 agenda-first calendar

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './v3-insights.css'
 import './v3-protocols.css'
 import './v3-history.css'
 import './v3-inventory.css'
+import './v3-calendar.css'
 import './qol-enhancements.js'
 import './structural-redesign.js'
 import './insights-build5.js'
@@ -23,6 +24,7 @@ import './v3-insights.js'
 import './v3-protocols.js'
 import './v3-history.js'
 import './v3-inventory.js'
+import './v3-calendar.js'
 
 /**
  * GitHub Pages and other static hosts often build without VITE_* secrets.
