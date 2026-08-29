6029d3e66e1e3d046f947c6f3b3a72350fd71fb2
ISORegret <rtaylorbrick@gmail.com>
2026-08-28 23:03:20 -0400

    Enable PepTalk 3.0 Insights redesign

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,13 @@ import './insights-build5.css'
 import './v3-foundation.css'
 import './v3-today.css'
 import './v3-weight.css'
+import './v3-insights.css'
 import './qol-enhancements.js'
 import './structural-redesign.js'
 import './insights-build5.js'
 import './v3-today.js'
 import './v3-weight.js'
+import './v3-insights.js'
 
 /**
  * GitHub Pages and other static hosts often build without VITE_* secrets.
